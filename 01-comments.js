d04bc42e68a70a0797bfe8600634840872d1984c
Cory Spears <coryspears@cory-spearss-macbook-pro.local>
2019-06-04 09:51:12 -0400

    firstcommit

---
+++ 01-comments.js
@@ -0,0 +1,20 @@
+//What comments do for us:
+//1) Communicate with other developers by writing in plain English
+//2) Annotate certain sections of our code for our future-selves
+//Comments may feel unncessary but the best way to understand code is to make a quick comment
+
+//console output of 'yay'
+console.log('yay!!');
+
+console.log('Day 1 is great!');
+
+console.log('How to bake bread below (Multiline comment');
+/* 
+Get Flour
+Get Eggs
+Mix Ingredients
+Knead Dough
+Let Dough Rise
+Bake
+Enjoy Bread
+*/
